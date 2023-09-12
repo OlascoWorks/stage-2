ea0c75233ab2de89f85579932205891404d69d89
OlascoWorks <samuelayodeji2478@gmail.com>
2023-09-12 12:46:50 +0100

    added secret key

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 db.init_app(app=app)
 
